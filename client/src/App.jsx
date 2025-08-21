616d606483f7df5bd470de0c68bf40f8af9d95a5
PTAE43 <kritsada1696@gmail.com>
2025-08-21 18:23:35 +0700

    feat: style css by tailwind step.1/2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
 
   //เพิ่มเติมข้อ Requirement โจทย์เสริม การคลิกปุ่ม หมวดหมู่ ให้เป็นคำค้นหา และเว้นวรรคคำ
   function updateSearch(arg) {
-    
+
     // onChange
     if (arg && arg.target) {
       setSearch(arg.target.value);
@@ -45,54 +45,64 @@ function App() {
 
 
   return (
-    <div className="App">
-      <div className="">เที่ยวไหนดี</div>
-      <div className="">ค้นหาที่เที่ยว</div>
-      <div className="w-[1200px]">
+    <div className="flex flex-col mx-auto md:w-[1200px]">
+
+      <div className="flex justify-center items-center mt-[50px] text-5xl text-sky-500">เที่ยวไหนดี</div>
+      <div className="flex md:px-20 text-left">ค้นหาที่เที่ยว</div>
+      <div className="p-2 md:px-20">
         <input
           type="text"
           value={search}
           onChange={updateSearch}
           placeholder="ค้นหาสถานที่เที่ยว..."
+          w
+          className="w-full p-2 text-center rounded-md border-2 shadow-2xs border-gray-100"
         ></input>
       </div>
-      <div className="">
-        <div className="flex flex-col">
-          {locations.map((item, index) => (
-            <li key={index}>
-              <div className="">
-                <div className="w-[300px] h-[300px]">
-                  <img src={item.photos[0]} alt={item.title} className="" />
-                </div>
-                <div className="flex flex-col">
-                  <div className="">{item.title}</div>
-                  <div className="">{item.description}</div>
-                  <div className="">{item.description}</div>
-                  <div className="">อ่านต่อ</div>
-                  <div className="">หมวดหมู่: {item.tags.map((tag, index) => (
-                    <button
-                      className="p-4 underline cursor-pointer"
-                      key={index}
-                      onClick={() => updateSearch(tag)}
-                    >
-                      {tag}
-                    </button>
-                  ))}</div>
-                  <div className="flex">{item.photos.slice(1, 4).map((photo, index) => (
-                    <img
-                      key={index}
-                      src={photo}
-                      alt={`photos-${index}-${item.title}`}
-                      className="w-[300px] h-[300px]" />
-                  ))}</div>
-                </div>
+
+      <div className="flex flex-col mt-2">
+        {locations.map((item, index) => (
+          <div key={index} className="my-6">
+            <div className="flex flex-col md:flex-row">
+
+              <div className="w-2/6">
+                <img src={item.photos[0]} alt={item.title} className="w-[350px] h-[240px] rounded-3xl object-cover shadow-2xs" />
               </div>
-            </li>
 
-          ))}
-        </div>
+              <div className="flex flex-col w-4/6">
+                <div className="text-2xl font-bold">{item.title}</div>
+                <div className="w-full my-2 line-clamp-3 md:line-clamp-1 md:overflow-hidden md:text-ellipsis">{item.description}</div>
+                <button
+                  className="flex text-sky-600 underline cursor-pointer"
+                  onClick={() => window.open(item.url, "_blank")}
+                >
+                  อ่านต่อ
+                </button>
+
+                <div className="">หมวดหมู่: {item.tags.map((tag, index) => (
+                  <button
+                    className="p-4 underline cursor-pointer"
+                    key={index}
+                    onClick={() => updateSearch(tag)}
+                  >
+                    {tag}
+                  </button>
+                ))}</div>
+
+                <div className="flex gap-4">{item.photos.slice(1, 4).map((photo, index) => (
+                  <img
+                    key={index}
+                    src={photo}
+                    alt={`photos-${index}-${item.title}`}
+                    className="w-[80px] h-[80px] rounded-2xl object-cover shadow-2xs" />
+                ))}</div>
+
+              </div>
+            </div>
+          </div>
+
+        ))}
       </div>
-
     </div>
   )
 
